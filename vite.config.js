4d6a01f22b81f4189fd1e0d081976e4892d1e205
sulis <sulistiopratomo@gmail.com>
2026-06-09 11:07:07 +0700

    fix(deploy): update base URL to /bangun-datar/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/mini_project_membuat_perhitungan_bangun_datar/',
+  base: '/bangun-datar/',
   plugins: [
     react(),
     tailwindcss(),
